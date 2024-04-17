b5b388636b7666eb8b21db73690d5336b4b8e5d7
Mario Gonzalez <marioivan.gonzalez@gmail.com>
2024-04-16 21:15:40 -0400

    funcion incremetar cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
+import { useState } from "react"
+
+import Guitar from "./components/Guitar"
+import Header from "./components/Header"
+import { db } from "./data/db"
 
 function App() {
   
+    //State
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
 
+    const MAX_QUANTITY = 5
+
+    const removeItemCart = (id) => {
+        setCart(prevCar => prevCar.filter(item => item.id !== id))
+    }
+
+    // Incrementar la cantidad de guitarras
+    const increseQuantity = (id) => {
+        const updatedCard = cart.map( item => {    // Primero creamos una copia del carrito
+            if(item.id === id && item.quantity < MAX_QUANTITY ){                     // Si el id del item es igual al id que estamos buscando
+                return {                            // retornamos un nuevo objeto con la cantidad incrementada
+                    ...item,                        // copiamos el item
+                    quantity: item.quantity + 1     // incrementamos la cantidad
+                }
+            }
+            return item                             // si no es el item que estamos buscando retornamos el item tal cual
+        })
+        setCart(updatedCard)                        // actualizamos el carrito
+    }
+
+    console.log(cart)
     return (
         <>
-            
+            <Header cart={cart} removeItemCart={removeItemCart} increseQuantity={increseQuantity} />
+
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">
+                    {data.map((guitar) => (
+                        <Guitar key={guitar.id} guitar={guitar} setCart={setCart} cart={cart}/>
+                    ))}         
+                </div>
+            </main>
+
+
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+                </div>
+            </footer>
         </>
     )
 }
---
+++ src/components/Guitar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Guitar = ({guitar, cart, setCart}) => {
+    
+    const {id, name, image, price, description} = guitar
+
+    const handleClick = (guitar) => {
+
+        const itemExists = cart.findIndex(item => item.id === guitar.id)
+        
+        if(itemExists >= 0) {
+            console.log('El item ya existe')
+            const updatedCart = [...cart]       // copiamos el carrito actual para no mutarlo
+            updatedCart[itemExists].quantity++  // aumentamos la cantidad del item
+            setCart(updatedCart)                // actualizamos el carrito
+        } else{
+            console.log('El item no existe!, agregando....')
+            guitar.quantity = 1
+            setCart([...cart, guitar])
+        }
+    }
+
+    return (
+        <>
+            <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+                <div className="col-4">
+                    <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
+                </div>
+                <div className="col-8">
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                    <p>{description}</p>
+                    <p className="fw-black text-primary fs-3">${price}</p>
+                    <button 
+                        type="button"
+                        className="btn btn-dark w-100"
+                        onClick={() => handleClick(guitar)} // setCartr([...cart, guitar])  // setCart(prevCart => [...prevCart, guitar]) *de esta forma no pasamos cart como prop
+                    >Agregar al Carrito</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Guitar
